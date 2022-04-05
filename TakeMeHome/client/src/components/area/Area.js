f41d4cb894ebf1f56920f3eda420c15a466849b8
Jaytwy24 <jtwy24@gmail.com>
2022-04-05 16:02:35 -0400

    collapsible list

--- TakeMeHome/client/src/components/area/Area.js
+++ TakeMeHome/client/src/components/area/Area.js
@@ -1,16 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardBody } from "reactstrap";
-import { Link } from "react-router-dom";
+import "../../App.css";
+
+
 
 const Area = ({ area }) => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
   return (
     <Card className="m-4">      
       <CardBody>
-      <strong> {area.name}</strong>
-      <ul>
-          {area.inventoryItems.map(i => <li key={i.id}> {i.name} </li>)}
-      </ul>
+      <button type="button" className="collapsible" onClick={() => setIsOpen(!isOpen)}> {area.name}</button>
+      <div className={isOpen ? 'd-flex justify-content-evenly flex-wrap content' : 'content'}>
+          {area.inventoryItems.map(i => <p className="p-2 bd-highlight" key={i.id}> {i.name} </p>)}
+      </div>
       </CardBody>
     </Card>
   );
